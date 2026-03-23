0da2b1a3bddba7814c354dd1506a9b38a1b24c26
Alexey Sharov <AskAlexSharov@gmail.com>
2026-03-23 09:22:45 +0700

    save

--- db/seg/seg_interface.go
+++ db/seg/seg_interface.go
@@ -27,14 +27,17 @@ const (
 const (
 	FileCompressionFormatV0 = uint8(0)
 	FileCompressionFormatV1 = uint8(1)
+	FileCompressionFormatV2 = uint8(2)
 )
 
 type FeatureFlag uint8
 
 const (
-	PageLevelCompressionEnabled FeatureFlag = 1 << iota // 0b001
-	KeyCompressionEnabled                               // 0b010
-	ValCompressionEnabled                               // 0b100
+	PageLevelCompressionEnabled    FeatureFlag = 1 << iota // 0b001
+	KeyCompressionEnabled                                  // 0b010
+	ValCompressionEnabled                                  // 0b100
+	WordLevelKeyCompressionEnabled                         // 0b1000
+	WordLevelValCompressionEnabled                         // 0b10000
 )
 
 type FeatureFlagBitmask uint8
